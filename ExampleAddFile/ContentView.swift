9eede2ec8cba5173fdc01793c528262d9777bceb
Satoshi Goto <hogerappa@gmail.com>
2025-06-30 20:40:23 +0900

    Initial commit

---
+++ ExampleAddFile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExampleAddFile
+//
+//  Created by mtgto on 2025/06/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExampleAddFile/ExampleAddFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAddFileApp.swift
+//  ExampleAddFile
+//
+//  Created by mtgto on 2025/06/30.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAddFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
